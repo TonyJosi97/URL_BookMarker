f8f26a15def0830f867c921fd9d21ec4432f838d
TonyJosi97 <tonyjosinew@gmai.com>
2018-06-19 23:33:19 +0530

    7 - Local Storage Test!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-function saveBookmark()
+function saveBookmark(e)
 {
-	//alert(1);
-	console.log('It works!');
+	//console.log('It works!');
+
+	//Get form values
+	var siteName = document.getElementById('siteName').value;
+    var siteURL = document.getElementById('siteURL').value;
+
+    var bookmarkObject = {
+    	name: siteName,
+    	url: siteURL
+    }
+
+
+    /*
+    localStorage.setItem('test', 'Hello World');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+	*/
+
+	
+
+	//Prevent form from submitting
+	e.preventDefault();
 }
