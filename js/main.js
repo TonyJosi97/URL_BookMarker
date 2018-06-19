3003d575ffa9f4a1bc0c200121d014810dba71c8
TonyJosi97 <tonyjosinew@gmai.com>
2018-06-19 23:50:48 +0530

    8 - Starts saving bookmarks to localstorage!

--- js/main.js
+++ js/main.js
@@ -20,7 +20,18 @@ function saveBookmark(e)
     localStorage.removeItem('test');
 	*/
 
-	
+	if(localStorage.getItem('bookmarks') === null)
+	{
+		var bookmarks = [];
+		bookmarks.push(bookmarkObject);
+		localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+	}
+	else
+	{
+		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+		bookmarks.push(bookmarkObject);
+		localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+	}
 
 	//Prevent form from submitting
 	e.preventDefault();
