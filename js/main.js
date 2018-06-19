2ab29ee6d9a2ba170b168a8a0e213ed4861a8d40
TonyJosi97 <tonyjosinew@gmai.com>
2018-06-20 00:44:15 +0530

    10 - Option to delete bookmark added

--- js/main.js
+++ js/main.js
@@ -33,12 +33,51 @@ function saveBookmarks(e)
 		localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
 	}
 
+	fetchBookmarks();
+
 	//Prevent form from submitting
 	e.preventDefault();
 }
 
+
+
+function deleteBookmark(url)
+{
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	for(var i = 0; i < bookmarks.length; i++)
+	{
+		if(bookmarks[i].url == url)
+			bookmarks.splice(i, 1);
+	}
+	localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+
+	fetchBookmarks();
+
+}
+
+
+
 function fetchBookmarks()
 {
 	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-	console.log(bookmarks);
+	//console.log(bookmarks);
+
+	var bookmarksResults = document.getElementById('bookmarksResults');
+
+	bookmarksResults.innerHTML = "";
+
+	for(var i = 0; i < bookmarks.length; i++)
+	{
+		var name = bookmarks[i].name;
+		var url = bookmarks[i].url;
+
+		bookmarksResults.innerHTML += '<div class="well">'+
+										'<h3>'+name+
+										'<a class = "btn btn-default" target="_blank" href="'+url+'">VISIT SITE</a>'+
+										'<a onclick = "deleteBookmark(\''+url+'\')" class = "btn btn-danger" href="#">DELETE BOOKMARK</a>'+
+										'</h3>'+
+										'</div>';
+
+	}
+
 }
